5bb2cd0aa23f838ca1d34d61db41fae9518657d3
fulong <fulong.mo@gmail.com>
2015-06-14 00:58:12 +0800

    first commit

---
+++ _admin/template/footer.php
@@ -0,0 +1,8 @@
+﻿	<div class="bottom">
+		<div id="copyright">	
+		&copy;2011 7eam - <a href='http://code.google.com/p/7ghost/'>官方网站</a> - <a href='http://www.7eam.com'>官方论坛</a> 
+		</div>
+	</div>
+</div><!-- end .doc -->
+</body>
+</html>
